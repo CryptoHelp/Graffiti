63f1c2e938675117548e425bdc76dfb59b716678
test@test.com <test@test.com>
2015-07-24 14:59:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Name: GRAFFITI
+Ticker: GRF
+Supply: 20.000.000 (20 Millions)
+Full-PoS
+PoS: 1%
+
---
+++ src/genesis.h
@@ -0,0 +1,12 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x0000028c563789020cba4af5ed22f7304baa615a1c370121fc334f1e5d55d5b7");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+#endif // GENESIS_H
